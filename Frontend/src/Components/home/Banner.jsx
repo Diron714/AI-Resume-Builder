8a5e8d161179d3bca30e74098b65fc8c6a927385
Diron714 <louisdiron2002@gmail.com>
2025-10-17 19:07:57 +0530

    refactor: Remove Banner component from Home page and clean up Hero component

--- Frontend/src/Components/home/Banner.jsx
+++ Frontend/src/Components/home/Banner.jsx
@@ -2,14 +2,7 @@ import React from 'react'
 
 function Banner() {
   return (
-    <div>
-
-        <div className="w-full py-2.5 font-medium text-sm text-purple-800 text-center bg-gradient-to-r from-[#080b06] to-[#000000]">
-            <p><span className="px-3 py-1 rounded-lg text-white bg-purple-600 mr-2">New</span>AI Website Builder</p>
-        </div>
-
-
-    </div>
+    <div>Banner</div>
   )
 }
 
--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -6,7 +6,7 @@ import Hero from '../Components/home/Hero'
 function Home() {
   return (
     <div>
-      <Banner />
+      
       <Hero />
       
     </div>
